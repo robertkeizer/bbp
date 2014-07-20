5ea5668aa59c151ccd07e7268b7eb2a3eab404d3
Robert Keizer <robert@keizer.ca>
2014-07-19 23:26:00 -0500

    Gutting. Starting with bignum.

--- bbp.js
+++ bbp.js
@@ -1,4 +1,7 @@
+var bignum = require( "bignum" );
+
 exports.bbp = function( s, b, m, a ){
+
 	return function( k ){
 		var self = this;
 
@@ -8,27 +11,6 @@ exports.bbp = function( s, b, m, a ){
 		} );
 
 
-		var part1 = 1/Math.pow(b,k);
-		console.log( "part1 is " + part1 );
-		
-		var part2 = [ ];
-		for( var j=0; j<m; j++ ){
-			part2.push( a[j] / Math.pow( ( ( m*k) + j ), s ) );
-			console.log( "part2[" + j + "] = " + part2[j] );
-		}
-
-		/*
-		var _ret = 0;
-		for( var j=0; j<m; j++ ){
-			console.log( "a[j] is " + a[j] );
-			console.log( "mk is " + (m*k) );
-			var mk = (m*k);
-			console.log( "j is " + j );
-			_ret = _ret * ( a[j] / Math.pow((mk+j),s) );
-		}
-
-		return (1 / Math.pow(b,k)) * _ret;
-		*/
 		return 1;
 	}
 }
--- test.js
+++ test.js
@@ -2,4 +2,4 @@ var bbp = require( "./bbp" );
 
 var pi = bbp.bbp(1,16,8,[4,0,0,-2,-1,-1,0,0]);
 
-console.log( pi( 1 ) );
+console.log( "pi at position 1 is : " + pi( 1 ) );
